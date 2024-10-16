27309e69872c407361a03d08933c93ab612aff93
成友慧 <76831998+lonely-ice@users.noreply.github.com>
2024-10-16 22:39:47 +0800

    Add files via upload

--- run2.py
+++ run2.py
@@ -1,4 +1,4 @@
-from quick_start_mine import run_recbole
+from recbole.quick_start.quick_start_mine import run_recbole
 
 run_recbole(model='SRGNN', dataset='yoochoose-buys', config_file_list=['test2.yaml'])
 
